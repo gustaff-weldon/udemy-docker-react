2b109f7c31122377135cec46ec9df3ac0257927c
gustaff-weldon <gustaff.weldon@gmail.com>
2021-04-02 17:41:55 +0200

    Update app text - fix tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there it works bar!
+          Testing, testing, is anybody out there?
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing, testing, is anybody out there?
+          Learn React.
         </a>
       </header>
     </div>

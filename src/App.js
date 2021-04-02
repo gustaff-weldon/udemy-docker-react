40f92008161e94720018c681622d79a2b5c79232
gustaff-weldon <gustaff.weldon@gmail.com>
2021-04-02 17:38:38 +0200

    Update app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React OR not
+          Testing, testing, is anybody out there?
         </a>
       </header>
     </div>
